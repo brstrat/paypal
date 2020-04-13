6c49d4b0a76c2473c06a3e07b978f1b1dec16149
Chris Baran <c.baran@brstrat.com>
2020-04-13 15:29:00 -0500

    Update setup to include 'api' scripts when installing

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='paypal',
     version='0.1.0',    
     description='Allows interacting with PayPal payments gateway',
     url='https://github.com/brstrat/paypal',
-    packages=['paypal'],
+    packages=find_packages(),
     install_requires=[
         'requests',                     
     ],
